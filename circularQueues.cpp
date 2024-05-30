d88b34f4327056dee63c054bc1d6c3e8d75644eb
wildanindi <wildanindi2@gmail.com>
2024-05-30 18:36:17 +0700

    menambah implementasi untuk melakukan iterasi atau perulangan dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -86,6 +86,16 @@ class Queues {
                     cout << queue_array[FRONT_position] << "    ";
                     FRONT_position++;
                 }
+
+                FRONT_position = 0;
+
+                // Iterasi dari awal array hingga REAR
+                while (FRONT_position <= REAR_position){
+                    cout << queue_array[FRONT_position] << "    ";
+                    FRONT_position++;
+                }
+                cout << endl;
             }
         }
 };
+
