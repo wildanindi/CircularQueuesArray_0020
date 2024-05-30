5c56d4bbf1906dc09b003760a3e47499ae673389
wildanindi <wildanindi2@gmail.com>
2024-05-30 18:32:53 +0700

    perbadingan perulangan dari front hingga array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -80,6 +80,12 @@ class Queues {
                 }
                 cout << endl;
             }
-           
+            else {
+                // JIka FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+                while (FRONT_position <= max - 1) {
+                    cout << queue_array[FRONT_position] << "    ";
+                    FRONT_position++;
+                }
+            }
         }
 };
