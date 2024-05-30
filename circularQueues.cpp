7a517b26103bb9058dca487394dd5a1ada2bcc03
wildanindi <wildanindi2@gmail.com>
2024-05-30 18:29:27 +0700

    menambah void display untuk menampilkan FRONT dan REAR position yang telah kita buat

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -60,5 +60,10 @@ class Queues {
             }
         }
 
-        
+        void display() {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+
+            
+        }
 };
