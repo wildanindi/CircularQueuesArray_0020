6cdb509fb236215e3a404fa003869e522bd9d88f
wildanindi <wildanindi2@gmail.com>
2024-05-30 17:39:31 +0700

    menambahkan implementasi untuk mengecek apakah antrian penuh atau tidak

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -17,5 +17,10 @@ class Queues {
             cin >> num;
             cout << endl;
 
+            // cek apakah antrian penuh
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)){
+                cout << "\nQueue overflow\n";
+                return;
+            }
         }
 };
