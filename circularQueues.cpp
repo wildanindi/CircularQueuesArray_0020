f3705800c557583eacb7281f6165747f2ab1f431
wildanindi <wildanindi2@gmail.com>
2024-05-30 18:32:14 +0700

    melakukan perbandingan lagi

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -72,6 +72,14 @@ class Queues {
 
             cout << "\nElements int he queue are....\n";
 
+            // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+            if (FRONT_position <= REAR_position) {
+                while (FRONT_position <= REAR_position){
+                    cout << queue_array[FRONT_position] << "    ";
+                    FRONT_position++;
+                }
+                cout << endl;
+            }
            
         }
 };
