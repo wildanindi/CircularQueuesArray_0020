5880e0567341a9336ec848fe165da00187c03f7f
wildanindi <wildanindi2@gmail.com>
2024-05-30 18:30:36 +0700

    seperti yang sebelumnya membuat implementasi untuk mengecek apakah antriannya kosong?

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -64,6 +64,14 @@ class Queues {
             int FRONT_position = FRONT;
             int REAR_position = REAR;
 
-            
+            // cek apakah antrian kosong
+            if (FRONT == -1) {
+                cout << "Queue is empty\n";
+                return;
+            }
+
+            cout << "\nElements int he queue are....\n";
+
+           
         }
 };
